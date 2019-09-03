2e08cc6395242748cdd35169d67f6ba97743a927
TGM - Thiago Mota <tgmspawn@gmail.com>
2019-09-03 04:50:00 -0300

    Máquina completa para o portinari

--- portinari/start.sh
+++ portinari/start.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
 
-target=/app
-
-if find "$target" -mindepth 1 -print -quit 2>/dev/null | grep -q .; then
-    echo "App exists, init server..."
+if find angular.json 2>/dev/null; then
+    echo "App exists"
 else
     echo "Copy template to app directory..."
-    cp /template/* /app -rf
-
-    echo "Install dependencies..."
-    #TODO: adicionar parâmetro para não entrar no build, e aguardar a instalação via Windows.
-    npm install
+    cp -rf /template/* /app/
 fi
 
-echo "Starting ng Server..."
-npm start
+if $INIT_NG_SERVER ; then
+    echo "Starting ionic Server..."
+    npm start
+else
+    bash
+fi
